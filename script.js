2d7eb50cd75bc3129b9dbfc0e6621a1de964b5d8
宮井伸也 <miyaishinya@miyaishinyanoMacBook-Air.local>
2026-01-26 07:12:53 +0900

    fix: Resolve SyntaxError by adding missing closing brace in resetGame() function

--- script.js
+++ script.js
@@ -185,6 +185,7 @@ function resetGame() {
         renderStore();
             console.log('Game Reset!');
         }
+    }
         
         function reincarnateGame() {
             if (confirm('【注意事項】転生すると現在のIssinと施設が全てリセットされます。累計1,000,000 Issin以上で転生ポイントを獲得できます。本当に転生しますか？')) {
